485a3de33e957b6ccf6bccf74275979a706a0793
LidiaGarrucho <lidiaxu.3@gmail.com>
2018-05-21 21:43:07 +0200

    Add script to add modifications in the image transforms

---
+++ transforms.py
@@ -0,0 +1,52 @@
+from __future__ import print_function, division
+from torchvision import transforms
+
+#  Method to compute the padding odf the input image to the max image size
+def get_padding(image, output_size):
+    output_max_width = output_size[0]
+    output_max_height = output_size[1]
+    w, h = image.size
+    pad_width = output_max_width - w
+    if pad_width < 2:
+        pad_left = pad_width
+        pad_right = 0
+    else:
+        if pad_width % 2 == 0:
+            pad_left = int(pad_width / 2)
+            pad_right = pad_left
+        else:
+            pad_left = int(pad_width / 2) + 1
+            pad_right = pad_left - 1
+
+    pad_height = output_max_height - h
+    if pad_height < 2:
+        pad_top = pad_height
+        pad_bottom = 0
+    else:
+        if pad_height % 2 == 0:
+            pad_top = int(pad_height / 2)
+            pad_bottom = pad_top
+        else:
+            pad_top = int(pad_height / 2) + 1
+            pad_bottom = pad_top - 1
+
+    padding = (pad_left, pad_top, pad_right, pad_bottom)
+    return padding
+
+# Class to perform the padding
+class PadImage(object):
+    """Pad the image in a sample to the max size
+
+    Args:
+        output_size (tuple or int): Desired output size.
+    """
+    def __init__(self, output_size):
+        assert isinstance(output_size, (int, tuple))
+        self.output_max_width = output_size[0]
+        self.output_max_height = output_size[1]
+
+    def __call__(self, image):
+        padding = get_padding(image, (self.output_max_width, self.output_max_height))
+        tsfm = transforms.Pad(padding)
+        image = tsfm(image)
+        return image
