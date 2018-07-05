f4d245c9b15f8311d4e9450556360799beff4c9f
Unknown <malrawi@cvc.uab.es>
2018-07-05 19:10:05 +0200

    Added a new file to contain some useful functions
    Word to label, this function is needed to find QbE and QbS

---
+++ utils/some_functions.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul  5 18:18:18 2018
+
+@author: malrawi
+"""
+import numpy as np
+
+def word_to_label(word_str):
+    d = {}
+    
+    count = 0
+    for i in word_str:
+      if i not in d:
+         d[i] = count
+         count += 1
+    
+    labels = [d[i] for i in word_str]
+    print("There are ", len(np.unique(labels)), "words" )
+    return labels
+    
