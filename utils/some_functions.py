9bca5cd6387ad74cbbd292403faf339f876a8bbe
Unknown <malrawi@cvc.uab.es>
2018-07-08 01:17:34 +0200

    Correcting LOCs

--- utils/some_functions.py
+++ utils/some_functions.py
@@ -6,6 +6,7 @@ Created on Thu Jul  5 18:18:18 2018
 @author: malrawi
 """
 import numpy as np
+import pandas as pd
 
 def word_to_label(word_str):
     d = {}
@@ -20,3 +21,11 @@ def word_to_label(word_str):
     print("There are ", len(np.unique(labels)), " unique words" )
     return labels
     
+def remove_single_words(word_str):
+    # find the locations of all single 'appearance word'
+    # loc =  (~pd.Series(word_str).duplicated(keep=False)).astype(int).tolist()
+    loc =  (pd.Series(word_str).duplicated(keep=False)).astype(int).tolist()
+#   To remove all non-duplicates:
+    s = pd.Series(word_str)
+    word_str =  s[s.duplicated(keep=False)].tolist()    
+    return word_str, loc
