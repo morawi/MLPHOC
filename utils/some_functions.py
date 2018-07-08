288c03b50b16eb513117da3cd830af4cf99baced
Unknown <malrawi@cvc.uab.es>
2018-07-08 14:53:51 +0200

    Split mAP to a new function

--- utils/some_functions.py
+++ utils/some_functions.py
@@ -7,6 +7,8 @@ Created on Thu Jul  5 18:18:18 2018
 """
 import numpy as np
 import pandas as pd
+from utils.retrieval import map_from_query_test_feature_matrices, map_from_feature_matrix
+import torch
 
 def word_to_label(word_str):
     d = {}
@@ -20,12 +22,30 @@ def word_to_label(word_str):
     labels = [d[i] for i in word_str]
     print("There are ", len(np.unique(labels)), " unique words" )
     return labels
+
     
 def remove_single_words(word_str):
-    # find the locations of all single 'appearance word'
-    # loc =  (~pd.Series(word_str).duplicated(keep=False)).astype(int).tolist()
+    # find the locations of all single 'appearance word'    
     loc =  (pd.Series(word_str).duplicated(keep=False)).astype(int).tolist()
-#   To remove all non-duplicates:
+#   Removing all non-duplicates:
     s = pd.Series(word_str)
     word_str =  s[s.duplicated(keep=False)].tolist()    
     return word_str, loc
+
+
+
+def find_mAP(word_str, pred, target, metric):
+    # for tyeps fo metric, see retrieval.py
+    # remove single wors from pred and target all
+    word_str, loc = remove_single_words(word_str)
+    loc = torch.ByteTensor(loc)
+    pred = pred[loc]  # we have to negate loc
+    target = target[loc]        
+    
+    query_labels = word_to_label(word_str)                 
+    mAP_QbE, avg_precs = map_from_query_test_feature_matrices(target, pred, 
+                                                          query_labels, query_labels,  metric)
+    mAP_QbS, avg_precs = map_from_feature_matrix(pred,query_labels,'cosine', False)
+    
+    return mAP_QbE,mAP_QbS
+    
