cf2ceef956ee5c35f80b6cd82e55f0fbd788f59e
Unknown <malrawi@cvc.uab.es>
2018-07-31 10:26:52 +0200

    Adding fore to background testing of wods

--- tests/test_a_loader.py
+++ tests/test_a_loader.py
@@ -30,8 +30,24 @@ def find_max_HW_in_data(data_set):
     print('max_h', max_h)
     print('max_w',max_w)
     
-    
-    
+
+def test_text_to_background(data_set):
+    '''
+    if the final histogram of the words is skewed to the right, 
+    then, uncomment img=max_gray-img 
+    '''
+    font = {'family' : 'normal', 'weight' : 'normal', 'size'   : 16}
+    matplot_rc('font', **font)
+    hh = []    
+    img, _, _ = data_set[0]; img_max = img.max()
+    print('max gray val is: ', img_max)
+    for i in range(len(data_set)):    
+        img, tt, ss = data_set[i]
+        # img = img_max  - img 
+        hh.append(img.sum()/(img.size* img_max))
+    # plt.hist(img.squeeze()) # to plot the hist of an image, rough one
+    plt.hist(hh)
+
 
 config_path = 'config/config_file_wg.py'
 configuration = Configuration(config_path, '')
@@ -40,34 +56,20 @@ cf.split_percentage = 1
 
 
 folder_of_data              = '/home/malrawi/Desktop/My Programs/all_data/'
-dataset_path_IFN              = folder_of_data + 'ifnenit_v2.0p1e/data/set_e/bmp/' # path to IFN images
-gt_path_IFN                   = folder_of_data + 'ifnenit_v2.0p1e/data/set_e/tru/' # path to IFN ground_truth 
+dataset_path_IFN              = folder_of_data + 'ifnenit_v2.0p1e/data/set_a/bmp/' # path to IFN images
+gt_path_IFN                   = folder_of_data + 'ifnenit_v2.0p1e/data/set_a/tru/' # path to IFN ground_truth 
 cf.dataset_path_IFN = dataset_path_IFN
 cf.gt_path_IFN = gt_path_IFN
 
 
-# data_set = IfnEnitDataset(cf, train=True, transform=None)
+data_set = IfnEnitDataset(cf, train=True, transform=None)
 # data_set = WashingtonDataset(cf, train=True, transform=None)
-cf.dataset_name = 'IAM'; data_set  = IAM_words(cf, mode='validate', transform = None, augmentation=True )
+# cf.dataset_name = 'IAM'; data_set  = IAM_words(cf, mode='validate', transform = None, augmentation = False )
 # thin_img = ImageThinning(img)
 
 # find_max_HW_in_data(data_set)
+# test_text_to_background(data_set)
 
-font = {'family' : 'normal',
-        'weight' : 'normal',
-        'size'   : 16}
-
-matplot_rc('font', **font)
-hh = []
-
-img, _, _ = data_set[0]; img_max = img.max()
-print('max gray val is: ', img_max)
-for i in range(len(data_set)):    
-    img, tt, ss = data_set[i]
-    # img = img_max  - img 
-    hh.append(img.sum()/(img.size* img_max))
-# plt.hist(img.squeeze()) # to plot the hist of an image, rough one
-plt.hist(hh)
 
 
 
