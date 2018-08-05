c210aac6b0c6bcd58a7b7e591b3b60eb28dd3c2c
Unknown <malrawi@cvc.uab.es>
2018-08-06 01:29:58 +0200

    Raw-hoc

---
+++ scripts/Word2RAWHOC.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug  4 01:55:19 2018
+
+@author: malrawi
+"""
+import numpy as np
+from scripts.Word2PHOC import build_phoc as build_phoc
+import sys
+
+def build_rawhoc(word, cf):
+    # phoc_unigrams =".0123456789abcdefghijklmnopqrstuvwxyz,-;':()£|"
+    no_symbols = len(cf.phoc_unigrams)
+    bitword_width = np.ceil(np.log2(no_symbols)).astype('uint8')
+    rawhoc = np.zeros(cf.rawhoc_repeates*bitword_width*cf.max_word_len)
+
+    for index, char in enumerate(word):
+        i = cf.phoc_unigrams.find(char)  # the one added to get rid of zero binary
+        if i < 0: print(char, 'not in unigrams '); sys.exit('Quiting')
+        bb = bin(i+1); bb=bb[2:]
+        bb = '0'*(bitword_width-len(bb))+bb  
+        bb = list(bb*cf.rawhoc_repeates)
+        bb = np.array(bb, dtype='uint8')
+        offset = index*cf.rawhoc_repeates*bitword_width
+        rawhoc[offset: offset + cf.rawhoc_repeates*bitword_width] = bb
+    
+    return rawhoc
+
+
+def build_pro_hoc(word, cf):
+    rawphoc =  build_rawhoc(word, cf)
+    phoc = build_phoc(word, cf)
+    p_raw_hoc = np.append(phoc, rawphoc)
+    return p_raw_hoc
+    
+    
