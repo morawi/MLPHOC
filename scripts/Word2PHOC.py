8c1e6fa4be29dc551add05aad1f6a4404e78420c
Unknown <malrawi@cvc.uab.es>
2018-08-08 13:22:25 +0200

    Adding PHOC Tolerance

--- scripts/Word2PHOC.py
+++ scripts/Word2PHOC.py
@@ -52,6 +52,7 @@ def build_phoc(word, cf): # alphabet = 'multiple', unigram_levels = [2,3,4,5]):
                         cf.phoc_unigrams) + region * len(cf.phoc_unigrams) + char_index
                     phoc[feat_vec_index] = 1
        
-    # phoc = [1-cf.tolerance if x>0.5 else cf.tolerance for x in phoc]
+    if cf.phoc_tolerance>0:
+        phoc = np.array([1-cf.tolerance if x>0.5 else cf.tolerance for x in phoc])
     return phoc 
 
