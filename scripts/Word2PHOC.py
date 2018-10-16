08d2f9b73246747e0d37ceafb2f06d12a7db59f0
Mohammed Al-Rawi <malrawi@cvc.uab.es>
2018-10-16 15:28:18 +0200

    the print function

--- scripts/Word2PHOC.py
+++ scripts/Word2PHOC.py
@@ -40,8 +40,8 @@ def build_phoc(word, cf): # alphabet = 'multiple', unigram_levels = [2,3,4,5]):
             if  cf.keep_non_alphabet_of_GW_in_analysis==False and cf.dataset_name=='WG':
                 continue
             else:    
-                print('The unigram \'%s\' is unknown', char)
-                logger.fatal('The unigram \'%s\' is unknown', char)
+                print('The unigram ', char, ' is unknown')
+               # logger.fatal('The unigram \'%s\' is unknown', char)
                 sys.exit(0)
 
         char_index = char_indices[char]
